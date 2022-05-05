824d9670085a96f9004e63de617a5481a7ba9ef8
Михаил Андреев <scorpik329@mail.ru>
2022-05-05 13:26:33 +0700

    version 1

---
+++ Vector/Vector/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This app works with vectors");
+            
+            Console.WriteLine("Vector1 : X = 1; Y = 2; Z = 3");
+            Vector vector1 = new Vector(1, 2, 3);
+            
+            Console.WriteLine("Vector2 : X = 4; Y = 5; Z = 6");
+            Vector vector2 = new Vector(4, 5, 6);
+
+            /*
+             * Расчет длин векторов
+             */
+            Console.WriteLine($"Vector1 Length : {vector1.VectorLength()}");
+            Console.WriteLine($"Vector2 Length : {vector2.VectorLength()}");
+
+            /*
+             * Расчет скалярного произведения
+             */
+            if (vector1.ScalarProduct(vector2) != null)
+                Console.WriteLine($"Scalar product Vector1 and Vector2 : {vector1.ScalarProduct(vector2)}");
+            else
+                Console.WriteLine("Scalar product Error");
+
+            /*
+             * Расчет угла между векторами
+             */
+            if (vector1.CosAngle(vector2) != null)
+                Console.WriteLine($"Cos of angle between Vector1 and Vector2 : {vector1.CosAngle(vector2)}");
+            else
+                Console.WriteLine("Angle calculation Error");
+
+
+            /*
+             * Расчет произведения между векторами
+             */
+            Calculation(vector1, vector2, Operations.Product);
+
+            /*
+             * Расчет суммы между векторами
+             */
+            Calculation(vector1, vector2, Operations.Add);
+            
+            /*
+             * Расчет разности между векторами
+             */
+            Calculation(vector1, vector2, Operations.Substract);
+
+            Console.ReadKey();
+        }
+
+        private static void Calculation(Vector vector1, Vector vector2, Operations oper)
+        {
+            Vector result1;
+            Vector result2;
+
+            if (vector1 == null || vector2 == null)
+                Console.WriteLine("Result2 calculation Error");
+
+            switch (oper)
+            {
+                case Operations.Product:
+                    result1 = vector1.Product(vector2);
+                    if (result1 != null)
+                        Console.WriteLine($"Product of Vector1 and Vector2 by method: X = {result1.X}, Y = {result1.Y}, Z = {result1.Z}");
+                    else
+                        Console.WriteLine("Product calculation Error");
+
+                    result2 = vector1 * vector2;
+                    Console.WriteLine($"Product of Vector1 and Vector2 by overload: X = {result2.X}, Y = {result2.Y}, Z = {result2.Z}");
+                    break;
+               
+                case Operations.Add:
+                    result1 = vector1.Add(vector2);
+                    if (result1 != null)
+                        Console.WriteLine($"Product of Vector1 and Vector2 by method: X = {result1.X}, Y = {result1.Y}, Z = {result1.Z}");
+                    else
+                        Console.WriteLine("Product calculation Error");
+
+                    result2 = vector1 + vector2;
+                    Console.WriteLine($"Product of Vector1 and Vector2 by overload: X = {result2.X}, Y = {result2.Y}, Z = {result2.Z}");
+                    break;
+               
+                case Operations.Substract:
+                    result1 = vector1.Substruct(vector2);
+                    if (result1 != null)
+                        Console.WriteLine($"Product of Vector1 and Vector2 by method: X = {result1.X}, Y = {result1.Y}, Z = {result1.Z}");
+                    else
+                        Console.WriteLine("Product calculation Error");
+
+                    result2 = vector1 - vector2;
+                    Console.WriteLine($"Product of Vector1 and Vector2 by overload: X = {result2.X}, Y = {result2.Y}, Z = {result2.Z}");
+                    break;
+            }
+
+
+        }
+
+    }
+}
---
+++ Vector/Vector/Vector.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+        public double Z;
+
+        public Vector(double? x, double? y, double? z)
+        {
+            X = x ?? 0.0;
+            Y = y ?? 0.0;
+            Z = z ?? 0.0;
+        }
+
+        public double VectorLength()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+
+        public double? ScalarProduct(Vector vector)
+        {
+            if (vector == null)
+            {
+                return null;
+            }
+            else
+            {
+                return X * vector.X + Y * vector.Y + Z * vector.Z;
+            }
+        }
+
+        public double? CosAngle(Vector vector)
+        {
+            var scalar = ScalarProduct(vector);
+            var length1 = VectorLength();
+            var length2 = vector?.VectorLength();
+            return (!scalar.HasValue) || (!length2.HasValue) || (length2.Value < 1e-10) ? null : scalar.Value / (length1 * length2.Value);
+        }
+
+        public Vector Product(Vector vector)
+        {
+            if (vector == null)
+            {
+                return null;
+            }
+            else
+            {
+
+                return new Vector(Y * vector.Z - Z * vector.Y, Z * vector.X - X * vector.Z, X * vector.Y - Y * vector.X);
+            }
+        }
+
+        public Vector Add(Vector vector)
+        {
+            if (vector == null)
+            {
+                return null;
+            }
+            else
+            {
+
+                return new Vector(X + vector.X, Y + vector.Y, Z + vector.Z);
+            }
+        }
+
+        public Vector Substruct(Vector vector)
+        {
+            if (vector == null)
+            {
+                return null;
+            }
+            else
+            {
+
+                return new Vector(X - vector.X, Y - vector.Y, Z - vector.Z);
+            }
+        }
+
+        /// <summary>
+        /// Перегрузка оператор '*' взамен метода Product
+        /// </summary>
+        /// <param name="vector1"></param>
+        /// <param name="vector2"></param>
+        /// <returns></returns>
+        public static Vector operator *(Vector vector1, Vector vector2)
+        {
+            return new Vector(
+                vector1.Y * vector2.Z - vector1.Z * vector2.Y, 
+                vector1.Z * vector2.X - vector1.X * vector2.Z, 
+                vector1.X * vector2.Y - vector1.Y * vector2.X);
+        }
+        
+        /// <summary>
+        /// Перегрузка оператор '+' взамен метода Add
+        /// </summary>
+        /// <param name="vector1"></param>
+        /// <param name="vector2"></param>
+        /// <returns></returns>
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+            return new Vector (vector1.X + vector2.X, vector1.Y + vector2.Y, vector1.Z + vector2.Z);
+        }
+
+        /// <summary>
+        /// Перегрузка оператор '-' взамен метода Substruct
+        /// </summary>
+        /// <param name="vector1"></param>
+        /// <param name="vector2"></param>
+        /// <returns></returns>
+        public static Vector operator -(Vector vector1, Vector vector2)
+        {
+            return new Vector(vector1.X - vector2.X, vector1.Y - vector2.Y, vector1.Z - vector2.Z);
+        }
+    }
+}
